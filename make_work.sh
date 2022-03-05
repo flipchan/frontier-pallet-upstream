56abe40ed01a0476aae1406f70916b74719b3a47
Filip <flipchan@riseup.net>
2022-03-05 21:52:27 +0200

    Revert "pallet"
    This reverts commit 9b63b9cc7fab8aaa82dbf85d3bc146bb0f9fb235.

--- make_work.sh
+++
@@ -1,9 +0,0 @@
-echo "switching from 17 to polkadot-v0.9.15"
-cargo update --aggressive
-sed -i 's/polkadot-v0.9.17/polkadot-v0.9.15/g' */Cargo.tml                      
-sed -i 's/polkadot-v0.9.17/polkadot-v0.9.15/g' client/*/Cargo.toml              
-sed -i 's/polkadot-v0.9.17/polkadot-v0.9.15/g' frame/*/Cargo.toml               
-sed -i 's/polkadot-v0.9.17/polkadot-v0.9.15/g' template/*/Cargo.toml            
-sed -i 's/polkadot-v0.9.17/polkadot-v0.9.15/g' frame/evm/precompile/*/Cargo.toml
-sed -i 's/polkadot-v0.9.17/polkadot-v0.9.15/g' primitives/*/Cargo.toml          
-cargo check --release
